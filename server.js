740a073cf6ff2326c4fab3c01c9231a47df1f3d7
Lukasz Marchewka <lukasz.marchewka@netbulls.eu>
2015-08-04 12:06:38 +0200

    Notification server working on socket.io

---
+++ server.js
@@ -0,0 +1,51 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var pushService = (function () {
+	var connections = {};
+	return {
+		addConnection: function (id, socket) {
+			socket.connectionId = id;
+			connections[id] = socket;
+		},
+		removeConnection: function (socket) {
+			if (socket.connectionId && connections[socket.conectionId]) {
+				delete connections[socket.connectionId];
+			}
+		},
+		pushMessage: function (to, message) {
+			if (connections[to]) {
+				connections[to].emit('message', message);
+			}
+		}
+	}
+}());
+
+io.on('connection', function (socket) {
+	socket.on('register', function (username) {
+		pushService.addConnection(username, socket);
+	});
+
+	socket.on('disconnect', function () {
+		pushService.removeConnection(socket);
+	});
+});
+
+app.post('/api/push', function (req, res) {
+	if (req.header('X-AUTH-TOKEN') != process.env['AUTH_TOKEN']) {
+		res.status(401).send();
+	} else if (req.body.user && req.body.message) {
+		pushService.pushMessage(req.body.user, req.body.message);
+		res.send();
+	}
+	else {
+		res.status(400).send('Bad Request');
+	}
+});
+
+http.listen(3000);

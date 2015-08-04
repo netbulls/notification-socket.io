f738ba8cf699f272f8f236d4cc343b5f4dae81e6
Lukasz Marchewka <lukasz.marchewka@netbulls.eu>
2015-08-04 12:23:49 +0200

    Notification server working on socket.io

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
@@ -47,6 +48,10 @@ app.post('/api/push', function (req, res) {
 	}
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
+var port = app.get('port');
+if (port === undefined) {
+	port = 3000;
+}
+app.listen(port, function () {
+	console.log('Node app is running on port', port);
 });
